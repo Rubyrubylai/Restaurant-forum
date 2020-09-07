2ce90f004b53b2067ce1fb40487420f0e5d89b3e
Ruby Li <r844312@gmail.com>
2020-09-07 22:28:26 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.listen('3000', () => console.log('app is listening!'))
