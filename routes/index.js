221953b6475eb6025960fcb2a6220d9acebe6a6d
Ruby Li <r844312@gmail.com>
2020-09-08 23:56:08 +0800

    Add adminRestaurant CRUD

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,34 @@ const userController = require('../controllers/userController')
 const auth = require('../config/auth')
 
 module.exports = (app, passport) => {
-    //使用者餐廳相關路由
+    //使用者相關路由
     app.get('/', auth.authenticated, (req, res) => { return res.redirect('/restaurants') })
+    //使用者餐廳相關路由
     app.get('/restaurants', auth.authenticated, restController.getRestaurants)
+    app.get('/restaurant/:id', auth.authenticated, restController.getRestaurant)
+    app.post('/restaurant', auth.authenticated, restController.postRestaurant)
+    app.put('/restaurant/:id', auth.authenticated, restController.editRestaurant)
+    app.delete('/restaurant/:id', auth.authenticated, restController.deleteRestaurant)
 
-    //admin餐廳相關路由
+
+    //admin相關路由
     app.get('/admin', auth.authenticatedAdmin, (req, res) => { return res.redirect('/admin/restaurants')})
+    
+    //admin餐廳相關路由
+    //總覽
     app.get('/admin/restaurants', auth.authenticatedAdmin, adminController.getRestaurants)
+    //瀏覽一筆
+    app.get('/admin/restaurant/:id', auth.authenticatedAdmin, adminController.getRestaurant)
+    //新增餐廳頁面
+    app.get('/admin/restaurant/create', auth.authenticatedAdmin, adminController.createRestaurant)
+    //新增餐廳
+    app.post('/admin/restaurants', auth.authenticatedAdmin, adminController.postRestaurant)
+    //編輯餐廳頁面
+    app.get('/admin/restaurant/edit', auth.authenticatedAdmin, adminController.editRestaurant)
+    //編輯餐廳
+    app.put('/admin/restaurant/:id', auth.authenticatedAdmin, adminController.putRestaurant)
+    //刪除餐廳
+    app.delete('/admin/restaurant/:id', auth.authenticatedAdmin, adminController.deleteRestaurant)
 
     //使用者相關路由
     app.get('/signup', userController.signupPage)
