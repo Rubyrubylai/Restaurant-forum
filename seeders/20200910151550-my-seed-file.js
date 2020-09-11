ed491491648964c0a8d05be9b6a462c9ec795c15
Ruby Li <r844312@gmail.com>
2020-09-11 22:53:34 +0800

    Update user password in seeder

--- seeders/20200910151550-my-seed-file.js
+++ seeders/20200910151550-my-seed-file.js
@@ -6,21 +6,21 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
-      password: bcrypt.hashSync('a123', bcrypt.genSaltSync(10), null),
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
       email: 'user1@example.com',
-      password: bcrypt.hashSync('a123', bcrypt.genSaltSync(10), null),
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
       email: 'user2@example.com',
-      password: bcrypt.hashSync('a123', bcrypt.genSaltSync(10), null),
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
       createdAt: new Date(),
