6971214daea287d6917342bdaf6ac7fbf8d68441
Ruby Li <r844312@gmail.com>
2020-09-13 00:43:49 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,20 +2,37 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
     getRestaurants: (req, res) => {
+        let offset = 0
         let whereQuery = {}
         let categoryId = ''
+        //若有點選分類
         if (req.query.categoryId) {
             categoryId = Number(req.query.categoryId)
             whereQuery['CategoryId'] = categoryId
         }
-        Restaurant.findAll({
+        //若有點選分頁
+        if (req.query.page) {
+            offset = (req.query.page - 1) * pageLimit
+        }
+        Restaurant.findAndCountAll({
             include: [Category],
-            where: whereQuery
+            where: whereQuery,
+            offset,
+            limit: pageLimit
         })
-        .then(restaurants => {
-            const data = restaurants.map(r => ({
+        .then(result => {
+            //分頁功能所需資料
+            let pages = Math.ceil(result.count / pageLimit)
+            let page = Number(req.query.page) || 1
+            let prev = page - 1 < 1 ? 1 : page - 1
+            let next = page + 1 > pages ? pages : page + 1
+            let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+            //顯示在畫面上的餐廳資料
+            const data = result.rows.map(r => ({
                 ...r.dataValues,
                 description: r.description.substring(0, 50),
                 categoryName: r.Category.name           
@@ -25,7 +42,7 @@ const restController = {
                 nest: true
             })
             .then(categories => {
-                return res.render('restaurants', { restaurants: data, categories, categoryId })
+                return res.render('restaurants', { restaurants: data, categories, categoryId, page, totalPage, prev, next })
             })
         })
     },
