d72e1681628e10aac38ea952ce01b7353fc3c31f
Ruby Li <r844312@gmail.com>
2020-09-12 09:48:53 +0800

    Update migrations

--- migrations/20200909161250-add-image-to-restaurants.js
+++ migrations/20200909161250-add-image-to-restaurants.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Restaurants', 'image', {
       type: Sequelize.STRING
-    });
+    })
   },
-
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Restaurants', 'image');
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Restaurants', 'image')
   }
-};
+}
+
